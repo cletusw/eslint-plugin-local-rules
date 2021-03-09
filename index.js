a0b87ad5903b11d14d73ed401a17715cec3948bd
Clayton Watts <cletusw@gmail.com>
2021-03-09 08:24:26 -0700

    Add comment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 var path = require('path');
 
+// Empty extension takes advantage of Node's default require behavior to check for
+// eslint-local-rules.js as well as an eslint-local-rules folder with an index.js
 var exts = ['', '.cjs'];
 var rules = requireUp('eslint-local-rules', __dirname);
 
