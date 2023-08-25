3bf31491ffd48547091980755200b91e36ba2558
takurinton <takurinton@takurinton.dev>
2023-08-26 01:02:40 +0900

    feat: Check CWD before plugin directory (#20)
    Fixes #17 
    
    This allows for an npm/yarn/pnpm workspace to override the eslint-local-rules of the project root.
    
    Co-authored-by: takurinton <takurinton@takurinton.dev>

--- index.js
+++ index.js
@@ -4,7 +4,13 @@
 var { requireUp } = require('./requireUp');
 var { DEFAULT_EXTENSIONS } = require('./constants');
 
-var rules = requireUp('eslint-local-rules', DEFAULT_EXTENSIONS, __dirname);
+// First check for local rules in the current working directory and its ancestors (enables npm/yarn/pnpm workspaces support)
+var rules = requireUp('eslint-local-rules', DEFAULT_EXTENSIONS, process.cwd());
+
+if (!rules) {
+  // Then try the directory containing this plugin and its ancestors
+  rules = requireUp('eslint-local-rules', DEFAULT_EXTENSIONS, __dirname);
+}
 
 if (!rules) {
   throw new Error(
@@ -13,6 +19,8 @@ if (!rules) {
       ['.js'].concat(DEFAULT_EXTENSIONS.filter(Boolean)) +
       '} ' +
       'or eslint-local-rules/index.js (checked all ancestors of "' +
+      process.cwd() +
+      '" and "' +
       __dirname +
       '").'
   );
